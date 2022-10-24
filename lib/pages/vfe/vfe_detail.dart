e49e25163d74b83f704a075454c867471d7efbbc
MZQ <zhiquan911@qq.com>
2022-10-24 15:44:07 +0800

    add vfe_detail

---
+++ lib/pages/vfe/vfe_detail.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class VFEDetailView extends StatefulWidget {
+  const VFEDetailView({Key? key}) : super(key: key);
+
+  @override
+  State<VFEDetailView> createState() => _VFEDetailViewState();
+}
+
+class _VFEDetailViewState extends State<VFEDetailView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
